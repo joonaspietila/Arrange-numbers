06ac4b56f89080ea4a1f7d8b441725212e8a8ed4
joonaspietila <56837862+joonaspietila@users.noreply.github.com>
2021-05-23 16:13:43 +0300

    update

---
+++ Numbers/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> li = new List<int>();
+            li.Add(4);
+            li.Add(1);
+            li.Add(3);
+            li.Add(2);
+
+            foreach (var item in li)
+            {
+                Console.WriteLine(item);
+            }
+
+            li.MySortFunction();
+
+            Console.WriteLine("After sorting");
+
+            foreach (var item in li)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    public static class MyListFunction
+    {
+        public static void MySortFunction(this List<int> li)
+        {
+            int temp;
+            for (int i = 0; i < li.Count; i++)
+            {
+                for (int j = 0; j < li.Count; j++)
+                {
+                    if (li[i] < li[j])
+                    {
+                        //swap
+                        temp = li[i];
+                        li[i] = li[j];
+                        li[j] = temp;
+                    }
+                }
+                Console.WriteLine(".......................");
+                foreach (var item in li)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine(".......................");
+            }
+        }
+    }
+}
